fc0bc961c29b1253e8a067f808906aa15528cccc
Chloe Alnaji <caedeal@gmail.com>
2015-11-13 11:18:09 -0700

    Added a .yml file

--- js/app.js
+++ js/app.js
@@ -1,4 +1,11 @@
 angular.module("memories-app", [])
-  .controller("MemoriesController", function($scope) {
-    
+  .controller("MemoriesController", function($scope, $http) {
+    $http({
+      method: 'GET',
+      url: 'http://g12-chloe-alnaji-memories.cfapps.io/api/v1/memories'
+    }).then(function successCallback(response) {
+        $scope.allMemories = response.data.rows;
+      }, function errorCallback(response) {
+        console.log(response);
+      });
   });
